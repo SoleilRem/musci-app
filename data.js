02a2434ebedb59ab50bf0fbebd214c913a221117
SoleilRem <87884866+SoleilRem@users.noreply.github.com>
2021-12-14 21:16:29 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,52 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: '../music/森永千才 - ミチノチモシーキミノキモチ.mp3',
+        artist: 'artist 1',
+        cover: '../music/tu/ji.jpg'
+		
+    },
+    {
+        name: 'song 2',
+        path: '../music/Dan Dan 心魅かれてく - 坂井泉水.mp3',
+        artist: 'artist 2',
+        cover: '../music/tu/10.jpg',
+		mv:'../music/shipin/longzhu.mp4'
+    },
+    {
+        name: 'song 3',
+        path: '../music/運命のルーレット廻して (转动命运之轮)-《名侦探柯南》TV动画第97-123集片头曲 - ZARD.mp3',
+        artist: 'artist 3',
+        cover: '../music/tu/9.jpg'
+    },
+	{
+	    name: 'song 4',
+	    path: '../music/七里香 - 周杰伦.mp3',
+	    artist: 'artist 4',
+	    cover: '../music/tu/7.jpg'
+	},
+	{
+	    name: 'song 5',
+	    path: '../music/听妈妈的话 - 周杰伦.mp3',
+	    artist: 'artist 5',
+	    cover: '../music/tu/6.jpg'
+	},
+	{
+	    name: 'song 6',
+	    path: '../music/等你下课(with 杨瑞代) - 周杰伦.mp3',
+	    artist: 'artist 6',
+	    cover: '../music/tu/11.jpg'
+	},
+	{
+	    name: 'song 7',
+	    path: '../music/晴天 - 周杰伦.mp3',
+	    artist: 'artist 7',
+	    cover: '../music/tu/12.jpg'
+	},
+	{
+	    name: 'song 8',
+	    path: '../music/龙卷风 - 周杰伦.mp3',
+	    artist: 'artist 8',
+	    cover: '../music/tu/13.jpg'
+	},
+]
